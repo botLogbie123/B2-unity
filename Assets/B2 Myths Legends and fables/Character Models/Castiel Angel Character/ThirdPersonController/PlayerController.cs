cbe02e651d8ec64fbc669b3755380c141ebebf8e
botLogbie123 <5263766@student.gloscol.ac.uk>
2026-04-24 16:21:23 +0100

    character controller

--- Assets/B2 Myths Legends and fables/Character Models/Castiel Angel Character/ThirdPersonController/PlayerController.cs
+++ Assets/B2 Myths Legends and fables/Character Models/Castiel Angel Character/ThirdPersonController/PlayerController.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 
 public class CharacterController : MonoBehaviour
 {
@@ -40,6 +41,38 @@ public class CharacterController : MonoBehaviour
             playerAnim.SetTrigger("Idle");
             walking = false;
             //steps1.SetActive(false);
+        } 
+        if (Input.GetKeyDown(KeyCode.S)) 
+        {
+            playerAnim.SetTrigger("Walkback");
+            playerAnim.ResetTrigger("Idle");
+            //Steps1.SetActive(true);
         }
-    }
+        if (Input.GetKeyUp(KeyCode.S)) 
+        {
+            playerAnim.ResetTrigger("Walkback");
+            playerAnim.SetTrigger("Idle");
+            //steps1.SetActive(false);
+        }
+
+        if (Input.GetKey(KeyCode.A)) 
+        { 
+            playerTrans.Rotate(0, -ro_speed * Time.deltaTime, 0);
+        }
+        if (Input.GetKey(KeyCode.D)) 
+        { 
+            playerTrans.Rotate(0, -ro_speed * Time.deltaTime, 0);
+        }
+        if (walking == true) 
+        {
+            if (Input.GetKeyDown(KeyCode.LeftShift)) 
+            {
+                //steps1.SetActive(false);
+                //steps2.SetActive(true);
+                w_speed = olw_speed;
+                playerAnim.ResetTrigger("run");
+                playerAnim.SetTrigger("Walk");
+            }
+        }
+    }   
 }
